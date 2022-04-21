ad178d214f10970acb0e36f79a2b442a9634cdc3
Tommie55555 <Tommie55555theboss@gmail.com>
2022-04-21 11:53:39 +0200

    everything changed basicaly
    Co-authored-by: sammenyb <sammenyb@users.noreply.github.com>
    Co-authored-by: Kiki5686 <Kiki5686@users.noreply.github.com>
    Co-authored-by: Tobias Mattsson <Klattraren@users.noreply.github.com>

--- project/main.py
+++ project/main.py
@@ -42,15 +42,41 @@ angle = 0
 rotate_cw = True
 rotation_swap_timer_max = 0
 rotation_swap_timer = 0
+route = []
 
+
+def identify_color(rgb): #do not use this function on people or we'll get sued
+    red = rgb[0]
+    green = rgb[1]
+    blue = rgb[2]
+    if sum(rgb) < 10:
+        return Color.BLACK
+    elif green >= 0.75 * red and red >= 0.75 * green and blue < 0.2 * (red + green):
+        return Color.YELLOW
+    elif red >= 0.75 * blue and blue >= 0.75 * red and green < 0.2 * (red + blue):
+        return Color.PURPLE
+    elif green > 0.25 * red and green < 0.6 * red and blue < 0.25 * red:
+        return Color.BROWN
+    elif red > 1.5 * (green + blue): # ORDER MATTERS! This should remain *after* checking for brown.
+        return Color.RED
+    elif green > 1.9 * red + 1.3 * blue:
+        return Color.GREEN
+    elif blue > 1.6 * red + 1.0 * green:
+        return Color.BLUE
+    else:
+        return Color.WHITE
 
 def robot_status(status):
     print("The robot is " + str(status) + "right now")
 
+def feedback(color,beep_duration, beep_frequency):
+    
+    speaker.beep(beep_frequency, beep_duration)
+    light.on(color)
+    return
+
 def avg(list_of_stuff):
     return sum(list_of_stuff) / len(list_of_stuff)
-
-
 
 
 def collision_avoidance():
@@ -61,68 +87,68 @@ def collision_avoidance():
         vehicle_detected = True  #initialize avoid sequence
 
     if vehicle_detected: #vehicle wheels rotate the wrong way
-        robot.turn(-20)
+        robot.turn(-70)
         robot.straight(-200)
-        robot.turn(40)
-        robot.straight(-200)
+        robot.turn(90)
+        robot.straight(-300)
     
 
     return
-#def color_prio(current_prio):
+
+'''def color_prio(current_prio):
+    current_color = left_light.color()
+    route.append(current_color)
+    # fortsätt till $pickup hittad
+    pick_up = input("pickup point?: ")
+    drop_off = input("drop off point?: ")
+   
+'''
+
+
 
 def angle_change(rgb_value):
-    return rgb_value/12
+    if rgb_value > 15:
+        return rgb_value/15
+    else:
+        return 40/(rgb_value + 1) #no division by zero
 
 
 
 def follow_line(colors):
-    robot_status(status="following line")
+    robot_status(status="following " + str(left_light.color()) + " line")
     global angle, speed, rotate_cw, rotation_swap_timer_max, rotation_swap_timer
     continue_driving = 0 # does this need to be an int?
     while continue_driving == 0:
         collision_avoidance()
         color_left = left_light.rgb()
         turnrate = angle_change(avg(color_left))
-        robot.drive(- (min(speed*(1/turnrate), 80)), angle*turnrate)
-        current_color = left_light.color()
+        robot.drive(- (min(8 + speed*(3/turnrate), 70)), angle*turnrate)
+        current_color = identify_color(color_left)
+        print("current color: " + str(current_color) + ", built-in detection of color: " + str(left_light.color()) + ", color rgb: " + str(color_left))
 
-        # if identify_color(color_left) in colors:
-        #     # check if right color is the correct next color here?
-        #     rotation_swap_timer_max = 5
-        #     rotation_swap_timer = rotation_swap_timer_max
-        #     angle = 0
-        #     speed = 50
-        # else:
-        #     rotation_swap_timer -= 1
-        #     if rotation_swap_timer < 0:
-        #         rotate_cw = not rotate_cw
-        #         rotation_swap_timer_max += 5
-        #         rotation_swap_timer = rotation_swap_timer_max
-        #     if rotate_cw:
-        #         robot.turn(2)
-        #     else:
-        #         robot.turn(-2)
-        
-        if len(colors) > 1 and current_color == colors[1]:
-            colors.pop(0)
 
         color_multiplier = 1
         if current_color == Color.YELLOW:
-            color_multiplier = 0.4
+            color_multiplier = 0.24
 
-        if 20 < avg(left_light.rgb()) * color_multiplier <= 100 and current_color != colors[0]: #vit
+        if len(colors) > 1 and current_color == colors[1]:
+            colors.pop(0)
+            robot.straight(120) #backwards
+            robot.turn(230) #turn doesn't work quite properly so this ain't degrees
+        elif 23 < avg(left_light.rgb()) * color_multiplier <= 100 and current_color != colors[0]:
             speed = 40
             angle = -10
-        elif 10 <= avg(left_light.rgb()) * color_multiplier <= 20 and current_color != colors[0]: #mellan svart och vit
+        elif 14 <= avg(left_light.rgb()) * color_multiplier <= 23 and current_color != colors[0]:
             speed = 70
             angle = 0
-        else: #svart
+        else:
             speed = 40
             angle = 10
 
         
-        
-        print("speed " + str(speed) + ", angle " + str(angle) + ", color rgb: " + str(color_left) + " = " + str(current_color))
+        route.append(current_color)
+        #print("route: " + str(route))
+        print("speed " + str(speed) + ", angle " + str(angle))
 
 def reports(instructions):
     print('instructions that will be performed: ', instructions)
@@ -130,7 +156,7 @@ def reports(instructions):
 def cranelift():
     print("motor angle " + str(crane_motor.angle()))
     crane_motor.run_angle(15, -60, wait=True)
-    crane_motor.run_angle(15, 60, wait=True)
+    # crane_motor.run_angle(15, 60, wait=True)
 
 def main():
     instructions = [Color.BLUE, Color.YELLOW, Color.BLUE]
@@ -138,9 +164,10 @@ def main():
     #cranelift()
     reports(instructions)
     while loop_continue == 0:
-        """ihuiedhcid"""
+        """ihuiedhcid()"""
         follow_line(instructions) #"yellow", "brown", "green", "blue", "red"
-        collision_avoidance()
+        
+
 
 
 if __name__ == '__main__':
