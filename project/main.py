4a0f8c6288ccf4c3097ba09f2192122a2ee821bb
Tobias Mattsson <tomt21@student.bth.se>
2022-05-16 16:06:55 +0200

    16/5-2022 Follow line working

--- project/main.py
+++ project/main.py
@@ -50,6 +50,7 @@ start_time = time.time()
 
 color_list =[]
 
+#def cal_color():
 
 reference_rgb = {"black": (4, 5, 2),  # zeroes are NOT allowed in the values of this list.
 "brown": (10, 6, 4),
@@ -185,85 +186,85 @@ def collision_avoidance():
 
 
 
-def follow_line(colors):
-    copy_colors_reverse = colors[::-1]
-    print(copy_colors_reverse)
-    #print(choosen_warehouse)
-    global angle, speed, color_list
-    continue_driving = 0 # does this need to be an int?
-    while continue_driving == 0:
-        collision_avoidance()
-        color_left = left_light.rgb()
-        if avg(color_left) == 0:
-            color_left = (1,0,0) #avoid div by 0
+# def follow_line(colors):
+#     copy_colors_reverse = colors[::-1]
+#     print(copy_colors_reverse)
+#     #print(choosen_warehouse)
+#     global angle, speed, color_list
+#     continue_driving = 0 # does this need to be an int?
+#     while continue_driving == 0:
+#         collision_avoidance()
+#         color_left = left_light.rgb()
+#         if avg(color_left) == 0:
+#             color_left = (1,0,0) #avoid div by 0
 
-        adj_color_left = [color_calibration[0] * color_left[0], color_calibration[1] * color_left[1], color_calibration[2] * color_left[2]]
-        for i in range(len(adj_color_left)):
-            adj_color_left[i] = max(adj_color_left[i], 1)
+#         adj_color_left = [color_calibration[0] * color_left[0], color_calibration[1] * color_left[1], color_calibration[2] * color_left[2]]
+#         for i in range(len(adj_color_left)):
+#             adj_color_left[i] = max(adj_color_left[i], 1)
 
-        color_list.append(adj_color_left)
-        if len(color_list) > 5:
-            color_list.pop(0)
-        med_value = median(color_list)
+#         color_list.append(adj_color_left)
+#         if len(color_list) > 5:
+#             color_list.pop(0)
+#         med_value = median(color_list)
 
-        med_color = identify_color(med_value)
-        current_color = identify_color(adj_color_left)
-
-
-        robot_status(status="Searching for " + str(colors) + ", and following " + str(current_color) + " line")
-
-        color_multiplier = 1 / avg(reference_rgb[identify_color(adj_color_left, True)])
-        # this always looks at the first one in the list!
-
-        if len(colors) >= 2 and current_color == colors[1]:
-            speed *= 0.15
-            angle *= 0.17
-        robot.drive(-speed, -angle)
-
-        if len(colors) == 1:
-            colors = copy_colors_reverse
-        elif current_color != "white" and current_color != colors[0] and current_color != colors[1]:
-            angle = 0
-            speed = 30
-        elif len(colors) > 1 and med_color == colors[1]:
-            colors.pop(0)
-            print("changing color - found ", current_color)
-            print(colors)
-            if colors[0] != "black":
-                ninety_degree_turn()
-            else:
-                print("detected black. Turning around.")
-                robot.turn(170)
-        elif current_color == "white":
-            angle = -26# * avg(color_left) * color_multiplier
-            speed = 55
-        elif 2.2 < avg(color_left) * color_multiplier and current_color != colors[0]:
-            angle = -12 * avg(color_left) * color_multiplier
-            speed = 40
-        elif 1.2 <= avg(color_left) * color_multiplier <= 2.2 and current_color != colors[0]:
-            angle = 0
-            speed = 45
-        else:
-            relative_brightness = (avg(color_left) * color_multiplier) ** 2
-            if relative_brightness < 0.8:
-                relative_brightness = 0.8
-            if current_color == "red_pink":
-                relative_brightness -= 0.2
-            angle = 75 / relative_brightness #how much the robot turns when colored line is detected
-
-            speed = min(50, 20 * relative_brightness - 58)
+#         med_color = identify_color(med_value)
+#         current_color = identify_color(adj_color_left)
 
 
-        ''''
-        if not current_color == "white" and len(route) == 0:
-            route.append(current_color)
-            print("route: " + str(route))
-        elif not current_color == "white" and (str(route[-1]) != str(current_color)):
-            route.append(current_color)
-            print("route: " + str(route))
-            print("Test route:" + str(route[-1]) + " , " + str(current_color))
-        print("speed " + str(speed) + ", angle " + str(angle))
-        '''
+#         robot_status(status="Searching for " + str(colors) + ", and following " + str(current_color) + " line")
+
+#         color_multiplier = 1 / avg(reference_rgb[identify_color(adj_color_left, True)])
+#         # this always looks at the first one in the list!
+
+#         if len(colors) >= 2 and current_color == colors[1]:
+#             speed *= 0.15
+#             angle *= 0.17
+#         robot.drive(-speed, -angle)
+
+#         if len(colors) == 1:
+#             colors = copy_colors_reverse
+#         elif current_color != "white" and current_color != colors[0] and current_color != colors[1]:
+#             angle = 0
+#             speed = 30
+#         elif len(colors) > 1 and med_color == colors[1]:
+#             colors.pop(0)
+#             print("changing color - found ", current_color)
+#             print(colors)
+#             if colors[0] != "black":
+#                 ninety_degree_turn()
+#             else:
+#                 print("detected black. Turning around.")
+#                 robot.turn(170)
+#         elif current_color == "white":
+#             angle = -26# * avg(color_left) * color_multiplier
+#             speed = 55
+#         elif 2.2 < avg(color_left) * color_multiplier and current_color != colors[0]:
+#             angle = -12 * avg(color_left) * color_multiplier
+#             speed = 40
+#         elif 1.2 <= avg(color_left) * color_multiplier <= 2.2 and current_color != colors[0]:
+#             angle = 0
+#             speed = 45
+#         else:
+#             relative_brightness = (avg(color_left) * color_multiplier) ** 2
+#             if relative_brightness < 0.8:
+#                 relative_brightness = 0.8
+#             if current_color == "red_pink":
+#                 relative_brightness -= 0.2
+#             angle = 75 / relative_brightness #how much the robot turns when colored line is detected
+
+#             speed = min(50, 20 * relative_brightness - 58)
+
+
+#         ''''
+#         if not current_color == "white" and len(route) == 0:
+#             route.append(current_color)
+#             print("route: " + str(route))
+#         elif not current_color == "white" and (str(route[-1]) != str(current_color)):
+#             route.append(current_color)
+#             print("route: " + str(route))
+#             print("Test route:" + str(route[-1]) + " , " + str(current_color))
+#         print("speed " + str(speed) + ", angle " + str(angle))
+#         '''
 
 def reports(instructions):
     print('instructions that will be performed: ', instructions)
@@ -274,32 +275,28 @@ def cranelift():
     crane_motor.run_angle(15, -10, wait=True)
     # crane_motor.run_angle(15, 60, wait=True)
 
-def main_tmp():
+def pick_up():
 
     #correction = (30-left_light.reflection())*2
     correction = left_light.reflection()
     print(correction)
-    if 50 < ultra_sensor.distance() < 100:
+    if :
             robot.drive(0,0)
 
     elif ultra_sensor.distance() < 50:
         robot.drive(20,0)
     else:
         if 40 <= correction <= 60: # Drives straight forward
-            robot.drive(-100,0)
+            robot.drive(-30,0)
 
         elif 30 < correction < 40: # ini Begining to detect color
-            robot.drive(-50,-5)
+            robot.drive(-30,-5)
 
         elif 20 < correction <= 30: # Starting to detect color
             robot.drive(-25,-20)
 
         elif correction <= 20: # Dark, at color, turn right a lot
-            robot.drive(-25,-200)
-            # passed_line = False
-            # lines_passed = 0
-            # while lines_passed != 5:
-            #     lines_passed, passed_line = drive_tmp(lines_passed,passed_line)
+            robot.drive(-25,-50)
 
             print('sharp')
 
@@ -313,30 +310,38 @@ def main_tmp():
             robot.drive(-10, 50)
 
 
-def drive_tmp(times_passed,passed_line, turn_angle):
+def drive_tmp(times_passed,passed_line, turn_angle, instructions):
     print(' ')
-    if int(times_passed) % 2 == 0:
-        print("turning left")
-        robot.drive(-100,turn_angle)
-        # direction_toggle = robot.drive(-50,20)
+    print(instructions)
+    print(instructions[1])
+    if identify_color(left_light.rgb()) == instructions[1] and len(instructions) > 1: # Checks for next path-color
+        instructions.pop(1)
+        ninety_degree_turn()
+        times_passed = 1
+        print("Does 90 degrees turn")
     else:
-        robot.drive(-100,-turn_angle)
-        print("turning right")
-        # direction_toggle = robot.drive(-50,-20)
-    #--------------------------------------------------
-    if passed_line == True and left_light.color() == Color.WHITE: # Passed line and on white
-        passed_line = False
-        turn_angle += 3
-        times_passed += 1
-        print("Passed line and on white")
-    elif passed_line == False and left_light.color() != Color.WHITE: # On line
-        print("On Line")
-        turn_angle = 60
-        passed_line = True
-    elif passed_line == False and left_light.color() == Color.WHITE: # On white, haven't passed
-        print("On white, haven't passed")
-        turn_angle +=3
-    print('Times: ',times_passed)
+        if int(times_passed) % 2 == 0:
+            print("turning left")
+            robot.drive(-100,turn_angle)
+            # direction_toggle = robot.drive(-50,20)
+        else:
+            robot.drive(-100,-turn_angle)
+            print("turning right")
+            # direction_toggle = robot.drive(-50,-20)
+        #--------------------------------------------------
+        if passed_line == True and left_light.color() == Color.WHITE: # Passed line and on white
+            passed_line = False
+            turn_angle += 15
+            times_passed += 1
+            print("Passed line and on white")
+        elif passed_line == False and left_light.color() != Color.WHITE: # On line
+            print("On Line")
+            turn_angle = 45
+            passed_line = True
+        elif passed_line == False and left_light.color() == Color.WHITE: # On white, haven't passed
+            print("On white, haven't passed")
+            turn_angle +=15
+        print('Times: ',times_passed)
     return times_passed,passed_line, turn_angle
 
 
@@ -347,19 +352,17 @@ def main():
     turn_angle = 60
     passed_line = False
     lines_passed = 0
-    instructions = [identify_color(left_light.rgb()), "olive_green", "blue"]
+    instructions = [identify_color(left_light.rgb()), "olive_green", "pink_red", "blue"]
+    print(instructions)
     loop_continue = 0
-    calibrate(left_light.rgb())
+    # calibrate(left_light.rgb())
     #cranelift()
     reports(instructions)
     while loop_continue == 0:
-        print("Reflection: ", left_light.reflection())
+        #print("Reflection: ", left_light.reflection())
         print("Color: ", left_light.color())
         """ihuiedhcid()"""
-        # follow_line(instructions)
-        # main_tmp()
-        # lines_passed, passed_line = drive_tmp(lines_passed,passed_line)
-        lines_passed, passed_line, turn_angle = drive_tmp(lines_passed, passed_line, turn_angle)
+        lines_passed, passed_line, turn_angle = drive_tmp(lines_passed, passed_line, turn_angle, instructions)
 
 if __name__ == '__main__':
     sys.exit(main())
